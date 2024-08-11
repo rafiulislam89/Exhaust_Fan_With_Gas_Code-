32edf08334dda2698fc2b295388004356d4c3064
Rafiul Islam <71514888+rafiulislam89@users.noreply.github.com>
2024-08-12 02:12:55 +0600

    Add files via upload

---
+++ Exhaust_Fan_With_Gas_Code.ino
@@ -0,0 +1,27 @@
+const int Sesnsor_Pin = A4; // Sensor Pin
+const int Fan_Pin = 7; // Fan Pin
+const int RedLED = 10; // Red LED Pin
+const int GreenLED = 11; // Green LED Pin
+
+void setup() 
+{
+  pinMode(Sesnsor_Pin,INPUT);  // Sensor Pin as a INPUT
+  pinMode(Fan_Pin,OUTPUT);  // Fan Pin as a OUTPUT
+  pinMode(RedLED,OUTPUT);  // Red LED Pin as a OUTPUT
+  pinMode(GreenLED,OUTPUT);  // Green LED Pin as a OUTPUT
+}
+void loop() 
+{
+  if(digitalRead(Sesnsor_Pin) == HIGH)  // If Sesnsor Detect the Smoke
+  {
+    digitalWrite(Fan_Pin,HIGH);  // Turn ON the Fan
+    digitalWrite(RedLED,HIGH);  // Turn ON the Red LED
+    digitalWrite(GreenLED,LOW);  // Turn OFF the Green LED
+  }
+  else
+  {
+    digitalWrite(Fan_Pin,LOW);  // Turn OFF the Fan
+    digitalWrite(RedLED,LOW);  // Turn OF the Red LED
+    digitalWrite(GreenLED,HIGH);  // Turn ON the Green LED
+  }
+}
